a458abe67a16b2ad187ab19921ec667145532213
SKonteye <skonteye2@gmail.com>
2023-08-20 15:44:05 +0000

    errorhandlingConcepts

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,17 +1,35 @@
 using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpLogging(opts =>
-opts.LoggingFields = HttpLoggingFields.RequestProperties);
-builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+//builder.Services.AddHttpLogging(opts =>
+//opts.LoggingFields = HttpLoggingFields.RequestProperties);
+//builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
 
+//WebApplication app = builder.Build();
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseHttpLogging();
+//}
+//app.MapGet("/", () => "Hello World!");
+//RouteHandlerBuilder routeHandlerBuilder = app.MapGet("/person", () => new Person("Andrew", "Lock"));
+//app.UseWelcomePage();
+//app.UseStaticFiles();
+//app.Run();
+//public record Person(string FirstName , string LastName);
+
+//**********************************//
+//Middleware pipeline for a minimal APIs application
+//WebApplication app = builder.Build();
+//app.UseDeveloperExceptionPage();
+//app.UseStaticFiles();
+//app.UseRouting();
+//app.MapGet("/", () => "Hello World");
+//app.Run();
 WebApplication app = builder.Build();
-if (app.Environment.IsDevelopment())
+app.UseDeveloperExceptionPage();
+if (!app.Environment.IsDevelopment())
 {
-    app.UseHttpLogging();
+    app.UseExceptionHandler("/error");
 }
-app.MapGet("/", () => "Hello World!");
-RouteHandlerBuilder routeHandlerBuilder = app.MapGet("/person", () => new Person("Andrew", "Lock"));
-
+app.MapGet("/error", () => "Sorry, an error occured");
 app.Run();
-public record Person(string FirstName , string LastName);
