681e292d8165eec69d36f527005782e5c11c25bb
SKonteye <skonteye2@gmail.com>
2023-08-21 13:54:50 +0000

    statuscode middleware usage

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using System.Collections.Concurrent;
+using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddHttpLogging(opts =>
@@ -82,32 +84,83 @@ var builder = WebApplication.CreateBuilder(args);
 //    }
 //}
 //*****************************************************************************************************************//
-WebApplication app = builder.Build();
+//WebApplication app = builder.Build();
 
-var _fruit = new ConcurrentDictionary<string, Fruit>();
-app.MapGet("/fruit", ()=> _fruit);
-app.MapGet("/fruit/{id}", (string id) =>
-_fruit.TryGetValue(id, out var fruit)
-? TypedResults.Ok(fruit)
-: Results.NotFound());
-app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
-_fruit.TryAdd(id, fruit)
-? TypedResults.Created($"/fruit/{id}", fruit)
-: Results.BadRequest(new
-{
-    id = " A fruit with this id already exists"
-}));
-app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
-{
-    _fruit[id] = fruit;
-    return Results.NoContent();
-});
-app.MapDelete("/fruit/{id}", (string id) =>
-{
-    _fruit.TryRemove(id, out _);
-    return Results.NoContent();
-});
-app.Run();
-record Fruit (string Name, int Stock);
+//var _fruit = new ConcurrentDictionary<string, Fruit>();
+//app.MapGet("/fruit", ()=> _fruit);
+//app.MapGet("/fruit/{id}", (string id) =>
+//_fruit.TryGetValue(id, out var fruit)
+//? TypedResults.Ok(fruit)
+//: Results.NotFound());
+//app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+//_fruit.TryAdd(id, fruit)
+//? TypedResults.Created($"/fruit/{id}", fruit)
+//: Results.BadRequest(new
+//{
+//    id = " A fruit with this id already exists"
+//}));
+//app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+//{
+//    _fruit[id] = fruit;
+//    return Results.NoContent();
+//});
+//app.MapDelete("/fruit/{id}", (string id) =>
+//{
+//    _fruit.TryRemove(id, out _);
+//    return Results.NoContent();
+//});
+//app.Run();
+//record Fruit (string Name, int Stock);
 //*****************************************************************************************************************//
 
+//WebApplication app = builder.Build();
+//app.MapGet("/teapot", (HttpResponse response) =>
+//{
+//    response.StatusCode = 418;
+//    response.ContentType = MediaTypeNames.Text.Plain;
+//    return response.WriteAsync("I'm a teapot!");
+//});
+//app.Run();
+//*****************************************************************************************************************//
+//Results.problem and Results.ValidationProblem
+//WebApplication app = builder.Build();
+
+//var _fruit = new ConcurrentDictionary<string, Fruit>();
+//app.MapGet("/fruit", ()=> 
+//_fruit);
+
+//app.MapGet("/fruit/{id}", (string id) =>
+//_fruit.TryGetValue(id, out var fruit)
+//? TypedResults.Ok(fruit)
+//: Results.Problem(statusCode: 404));
+
+//app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+//_fruit.TryAdd(id, fruit)
+//? TypedResults.Created($"/fruit/{id}", fruit)
+//: Results.ValidationProblem(new Dictionary<string, string[]>
+//{
+//    {"id", new[] {"A fruit with this id already exists"} }
+//}));
+//app.Run();
+//record Fruit (string Name, int Stock);
+//*****************************************************************************************************************//
+//Configuring ExceptionHandler
+//builder.Services.AddProblemDetails();
+//WebApplication app = builder.Build();
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler();
+//}
+//app.MapGet("/", void () => throw new Exception());
+//app.Run();
+//*****************************************************************************************************************//
+//Using StatusCodeMiddleware
+builder.Services.AddProblemDetails();
+WebApplication app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+app.UseStatusCodePages();
+app.MapGet("/", ()=> Results.NotFound());
+app.Run();
