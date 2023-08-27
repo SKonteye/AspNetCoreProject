b2b1a8e348f8324ab8703ba541cdef08a711675e
SKonteye <skonteye2@gmail.com>
2023-08-27 13:41:31 +0000

    Endpoint filters in minimal api

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -155,12 +155,87 @@ var builder = WebApplication.CreateBuilder(args);
 //app.Run();
 //*****************************************************************************************************************//
 //Using StatusCodeMiddleware
-builder.Services.AddProblemDetails();
+//builder.Services.AddProblemDetails();
+//WebApplication app = builder.Build();
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler();
+//}
+//app.UseStatusCodePages();
+//app.MapGet("/", ()=> Results.NotFound());
+//app.Run();
+//*****************************************************************************************************************//
+//basic validation in minimal api
+//WebApplication app = builder.Build();
+//var _fruit = new ConcurrentDictionary<string, Fruit>();
+//app.MapGet("/fruit{id}", (string id) =>
+//{
+//    if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+
+//    {
+//        return Results.ValidationProblem(new Dictionary<string, string[]>
+//        {
+//            {"id", new[] {"Invalid format. Id must start with 'f'"} }
+//        });
+//    }
+//    return _fruit.TryGetValue(id, out var fruit)
+//        ? TypedResults.Ok(fruit)
+//        : Results.Problem(statusCode: 404);
+//});
+//app.Run();
+//record Fruit(string Name, int Stock);
+//*****************************************************************************************************************//
+//adding multiple filters to the endpoint filter pipeline
 WebApplication app = builder.Build();
-if (!app.Environment.IsDevelopment())
+var _fruit = new ConcurrentDictionary<string, Fruit>();
+app.MapGet("/fruit{id}", (string id) =>
+_fruit.TryGetValue(id, out var fruit)
+? TypedResults.Ok(fruit)
+: Results.Problem(statusCode: 404))
+    .AddEndpointFilter(ValidationHelper.ValidateId);
+    //.AddEndpointFilter(async (context, next) =>
+    //{
+    //    app.Logger.LogInformation("Executing filter...");
+    //    object? result = await next(context);
+    //    app.Logger.LogInformation($"Handler result:{result}");
+    //    return result;
+    //});
+app.MapGet("/fruit", () => _fruit);
+app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+_fruit.TryAdd(id, fruit)
+? TypedResults.Created($"/fruit/{id}", fruit)
+: Results.BadRequest(new
 {
-    app.UseExceptionHandler();
-}
-app.UseStatusCodePages();
-app.MapGet("/", ()=> Results.NotFound());
+    id = " A fruit with this id already exists"
+}));
+app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+{
+    _fruit[id] = fruit;
+    return Results.NoContent();
+});
+app.MapDelete("/fruit/{id}", (string id) =>
+{
+    _fruit.TryRemove(id, out _);
+    return Results.NoContent();
+});
 app.Run();
+record Fruit(string Name, int Stock);
+class ValidationHelper
+{
+    internal static async ValueTask<object?> ValidateId(
+        EndpointFilterInvocationContext context,
+        EndpointFilterDelegate next)
+    {
+        var id = context.GetArgument<string>(0);
+        if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+        {
+            return Results.ValidationProblem(
+                new Dictionary<string, string[]>
+                {
+                    {"id", new[] {"Invalid format. Id must start with 'f'"} }
+                });
+        }
+
+        return await next(context);
+    }
+}
