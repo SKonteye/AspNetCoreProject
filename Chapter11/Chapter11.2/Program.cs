9007a07dd7574fa6469925f61299fde9951fc9e2
SKonteye <skonteye2@gmail.com>
2023-09-24 15:04:22 +0000

    chapter 11 openAPI

---
+++ Chapter11/Chapter11.2/Program.cs
@@ -0,0 +1,35 @@
+using System.Collections.Concurrent;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var _fruit = new ConcurrentDictionary<string, Fruit>();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/fruit/{id}", (string id) =>
+    _fruit.TryGetValue(id, out var fruit)
+    ? TypedResults.Ok(fruit)
+    : Results.Problem(statusCode: 400))
+    .WithTags("fruit")
+    .Produces<Fruit>()
+    .ProducesValidationProblem(404);
+app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+     _fruit.TryAdd(id, fruit)
+     ? TypedResults.Created($"/fruit/{id}", fruit)
+     : Results.ValidationProblem(new Dictionary<string, string[]>
+     {
+         { "id", new[] { "A fruit with this id already exists"} }
+     }))
+    .WithTags("fruit")
+    .Produces<Fruit>(201)
+    .ProducesValidationProblem()
+    ;
+
+app.Run();
+record Fruit(string Name, int Stock);
---
+++ Chapter11/ConsoleClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Fruit;
+
+IFruitClient client = new FruitClient(
+    new HttpClient()
+    {BaseAddress =
+    new Uri("https://localhost:7232")
+    });
+
+Fruit.Fruit created = await client.FruitPOSTAsync("123",
+    new Fruit.Fruit { Name = "Banana", Stock = 100 });
+Console.WriteLine($"Created {created.Name}");
+
+Fruit.Fruit fetched = await client.FruitGETAsync("123");
+Console.WriteLine($"Fetched {fetched.Name}");
